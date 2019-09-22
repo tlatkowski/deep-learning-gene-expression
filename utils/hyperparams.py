575f87c674274d15797e41176bb516e210da7a4b
Tomasz Latkowski <tlatkowski@gmail.com>
2019-09-22 17:10:08 +0200

    moved yml config to hp

--- utils/hyperparams.py
+++ utils/hyperparams.py
@@ -1,22 +1,30 @@
+import io
+
+import yaml
+
+
 class Hyperparameters:
-  learning_rate = 0.001
-  input_size = 100
-  hidden_sizes = [80]
-  output_size = 1
-  num_features = 100
-  activation_function = 'tanh'
-  # TODO add force feature selection
-  # TODO gradient descent with momoentum
-  # TODO decorators for plots
-  num_layers = len(hidden_sizes) + 1
-  selection_methods = ['fisher', 'corr', 'ttest', 'random']
-  
-  num_epochs = 10000
-  batch_size = 20  # online learning when batch_size=1
-  cross_validation_folds = 10  # TODO when === num of observations then leave-one-out is applied.
-  lambda_reg = 0.8
-  norm_data = True
-  # TODO plot cost with and without reguralization
-  
-  # dirs:
-  data_file = 'data/data.tsv'
+  def __init__(self, path_to_config_file):
+    with io.open(path_to_config_file) as file:
+      config = yaml.load(file)
+    self.learning_rate = 0.001
+    self.input_size = 100
+    self.hidden_sizes = [80]
+    self.output_size = 1
+    self. num_features = 100
+    self.activation_function = 'tanh'
+    # TODO add force feature selection
+    # TODO gradient descent with momoentum
+    # TODO decorators for plots
+    self.num_layers = len(self.hidden_sizes) + 1
+    self.selection_methods = ['fisher', 'corr', 'ttest', 'random']
+    
+    self.num_epochs = 10000
+    self.batch_size = 20  # online learning when batch_size=1
+    self.cross_validation_folds = 10  # TODO when === num of observations then leave-one-out is applied.
+    self.lambda_reg = 0.8
+    self.norm_data = True
+    # TODO plot cost with and without reguralization
+    
+    # dirs:
+    self.data_file = 'data/data.tsv'
